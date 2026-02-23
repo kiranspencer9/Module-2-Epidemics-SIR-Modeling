23daaf544cf9fb95dad0a71364115616e13046e3
kiranspencer9 <kiranvahanspencer@gmail.com>
2026-02-22 23:41:51 -0500

    DATA Release 1 - Kiran
    graphed the day vs. active infections from data release 1 in a python file and then added the python script and graph to the jupyter notebook

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -4,7 +4,14 @@ import matplotlib.pyplot as plt
 
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv('C:\\Users\\kiran\\Desktop\\UVA\\Classes\\Semester 4\\Computational BME\\Module-2-Epidemics-SIR-Modeling\\Data\\mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 
 #%%
-# Make a plot of the active cases over time
+# Plot
+plt.figure() # Create a new figure
+plt.plot(data["day"], data["active reported daily cases"]) # Plot active infections vs day
+plt.xlabel("Day") # Label for x-axis
+plt.ylabel("Active Infections") # Label for y-axis
+plt.title("DATA RELEASE #1: Active Infections vs Day") # Title for the plot
+plt.grid(True) # Add a grid for better visibility
+plt.show()
